8d5ea4b2411e1180f3745ec65a29e1b7e9ea6981
Amar Vashishth <vashishth.amar@gmail.com>
2024-10-12 02:26:20 +0900

    feat: batch insertion of multiple items for a table

--- src/repository/persistence.rs
+++ src/repository/persistence.rs
@@ -30,25 +30,27 @@ pub fn add_items_to_table(
     table_number: u8,
     items_name: Vec<String>,
 ) {
+    let mut values = Vec::new();
     for name in items_name {
         if name.replace(' ', "").trim().is_empty() {
             continue;
         }
         let timestamp = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+        let prepare_mins = rand::thread_rng().gen_range(5..16);
 
-        pool.get_conn().unwrap().exec_drop(
-            r"INSERT INTO table_items (
-                    table_id, item_name, ordered_on, prepare_mins
-                   ) VALUES (:table_id, :item_name, :ordered_on, :prepare_mins)",
-            mysql::params! {
-                "table_id" => table_number,
-                "item_name" => name.clone(),
-                "ordered_on" => timestamp,
-                "prepare_mins" => rand::thread_rng().gen_range(5..16),
-            },
-        ).expect(format!(
-            "Failed: Unable to add item {} to table_number {}",
-            name, table_number).as_str()
-        );
+        values.push(format!(
+            "({}, '{}', '{}', {})",
+            table_number,
+            name,
+            timestamp,
+            prepare_mins
+        ));
     }
+
+    let query = format!(
+        "INSERT INTO table_items (table_id, item_name, ordered_on, prepare_mins) VALUES {}",
+        values.join(", ")
+    );
+    let mut conn = pool.get_conn().unwrap();
+    conn.query_drop(query).expect("Failed to insert items");
 }
