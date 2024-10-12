ef0ff220d2e5981aa7e5d6f5f97cba5371870cf7
Amar Vashishth <vashishth.amar@gmail.com>
2024-10-13 05:37:48 +0900

    chore: changed column names in db.sql

--- db.sql
+++ db.sql
@@ -1,7 +1,7 @@
 CREATE TABLE table_items (
   item_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  table_id INT NOT NULL,
+  table_number INT NOT NULL,
   item_name VARCHAR(25) NOT NULL,
-  prepare_mins INT NOT NULL,
+  prepare_minutes INT NOT NULL,
   ordered_on DATETIME NOT NULL
 ) ENGINE = InnoDB CHARACTER SET = (null) COLLATE = latin1_swedish_ci;
