271513df2afac688b94bec1f424d93f2d45de3ac
Amar Vashishth <vashishth.amar@gmail.com>
2024-10-15 02:23:24 +0900

    feat: increased table item name length

--- db_init/schema.sql
+++ db_init/schema.sql
@@ -5,7 +5,7 @@ USE restaurant;
 CREATE TABLE table_items (
   item_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   table_number INT UNSIGNED NOT NULL,
-  item_name VARCHAR(25) NOT NULL,
+  item_name VARCHAR(127) NOT NULL,
   prepare_minutes INT UNSIGNED NOT NULL,
   ordered_on DATETIME NOT NULL
 );
